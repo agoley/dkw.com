1f7aa3faa6323716c46e8e441ac377ef3f5ad776
Kris Redding <kredding@autoscribe.com>
2017-02-23 16:08:55 -0500

    add data service for content data

--- app/config/app.js
+++ app/config/app.js
@@ -2,6 +2,7 @@
 
 var routes = angular.module('routes', ['ui.router']);
 var directives = angular.module('directives', []);
+var services = angular.module('services', []);
 var components = angular.module('components', ['ui.bootstrap', 'ngAnimate', 'ngSanitize']);
 
 var dkwSite = angular.module('DKWSite',
@@ -9,5 +10,6 @@ var dkwSite = angular.module('DKWSite',
 										'ngAnimate',
 										'ui.router',
 										'directives',
+										'services',
 										'components',
 										'routes']);
