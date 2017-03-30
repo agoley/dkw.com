cf8006fe5194b06baec45c317c8212b0f56b4fde
Kris Redding <kredding@autoscribe.com>
2017-03-30 13:02:57 -0400

    update social media links

--- app/components/all.js
+++ app/components/all.js
@@ -7,17 +7,27 @@ components.component('all', {
 		ctrl.view = {};
 		ctrl.view.isMobile = false;
 		ctrl.view.isSearching = false;
-		
+    ctrl.socialMediaLinks = [
+      {"name":"facebook", "colorClass":"color1", "icon":"fa-facebook","url":"https://www.facebook.com/DKWcommunications/"},
+      {"name":"twitter", "colorClass":"color2", "icon":"fa-twitter","url":"https://twitter.com/dkw_comminc"},
+      {"name":"youtube", "colorClass":"color3", "icon":"fa-youtube","url":"https://www.youtube.com/channel/UCciaY_nt7U8GkZklOXLtWDA"},
+      {"name":"linkedin", "colorClass":"color4", "icon":"fa-linkedin-square","url":"https://www.linkedin.com/company/dkw-communications-inc-"}
+    ];
+    // Open Social Media Links
+    ctrl.openLink = function(url){
+
+    }
+
 		// set mobile to true
 		var configForMobile = function () {
 			ctrl.view.isMobile = true;
 		}
-		
+
 		// set mobile to false
 		var configForDesktop = function () {
 			ctrl.view.isMobile = false;
 		}
-		
+
 		// logic to configure the site for mobile oe desktop
 		var configView = function () {
 			if (window.innerWidth < 715) {
@@ -26,12 +36,12 @@ components.component('all', {
 				configForDesktop();
 			}
 		}
-		
+
 		configView();
-		
+
 		window.onresize = function(){
     		setTimeout(function(){
-				configView(); 
+				configView();
 				$scope.$apply();
 			}, 100);
 		};
